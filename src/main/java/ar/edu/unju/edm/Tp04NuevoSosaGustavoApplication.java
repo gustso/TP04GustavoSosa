7181cf8e0543b4bb754cfa43177fdb103f409ccf
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-05-11 07:18:08 -0700

    Inicializacion del Proyecto TP04

---
+++ src/main/java/ar/edu/unju/edm/Tp04NuevoSosaGustavoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04NuevoSosaGustavoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04NuevoSosaGustavoApplication.class, args);
+	}
+
+}
