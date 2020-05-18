2f7141087f5d3a9bf3110f3698cefbe6fd38949f
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-05-18 08:11:04 -0700

    Agregados de la clase 18 de Mayo

---
+++ src/main/java/ar/edu/unju/edm/controller/AplicacionController.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AplicacionController {
+
+	@GetMapping("/home")
+	public String iniciarAplicacion() {
+		return "index";
+	}
+}
---
+++ src/main/java/ar/edu/unju/edm/model/EquipoFutbol.java
@@ -0,0 +1,66 @@
+package ar.edu.unju.edm.model;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EquipoFutbol implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5528729956805132041L;
+	/**
+	 * 
+	 */
+	
+	private String nombre;	
+	
+	public EquipoFutbol() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EquipoFutbol(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EquipoFutbol other = (EquipoFutbol) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "EquipoFutbol [Capo del Norte=" + nombre + "]";
+	}	
+	
+}
